3c641b102c6618e71680a8a6302be0d93c9e441b
Timz Owen <34624703+TimzOwen@users.noreply.github.com>
2019-05-30 18:11:27 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/clubregistration/MainActivity.java
+++ app/src/main/java/com/example/clubregistration/MainActivity.java
@@ -4,10 +4,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    
+      EditText etUserName, etPassword;
+    Button btnLogin;
+    TextView tvRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+          //call the find view by ids Methods
+        findIds();
+
+        //set a click listener to the btn RegisterActivity
+        btnLogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //move to a new intent
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+    //function ot find Ids
+    public void findIds()
+    {
+        etUserName = (EditText) findViewById(R.id.etUserName);
+        etPassword = (EditText) findViewById(R.id.etUserPassword);
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        tvRegister = (TextView)findViewById(R.id.tvRegister);
+
+    }
     }
 }
